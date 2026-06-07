56623c038e28bc1d399493238306d19a0acd8c05
Ajay <ajaymk22@vt.edu>
2026-06-07 00:37:16 -0400

    feat: add Instagram link to footer

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,7 @@ export const site = {
     github: 'https://github.com/ajay-mk',
     scholar: 'https://scholar.google.com/citations?hl=en&user=VC-HIpUAAAAJ',
     orcid: 'https://orcid.org/0000-0002-0079-5443',
+    instagram: 'https://instagram.com/ajaymk_',
   },
 } as const;
 
